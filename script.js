b20d98b8a499d45d096e283baf34622c8da8ca39
Ryan Joo <ryanjooruian18@gmail.com>
2023-12-14 20:28:01 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 document.getElementById("scroll-to-bottom-button").addEventListener("click", function() {
 
     document.body.scrollIntoView({ behavior: "smooth", block: "end" });
     
     });
->>>>>>> 87ac10f3506a16dac0e3937ab267d8d786cc373d
