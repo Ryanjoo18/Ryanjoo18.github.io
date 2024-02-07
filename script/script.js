ad2748c9feb846965114608860b0c1f09fe12859
Ryan Joo <ryanjooruian18@gmail.com>
2024-02-07 19:50:39 +0800

    Update css

--- script/script.js
+++ script/script.js
@@ -1,29 +1,12 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-
-closeBtn.addEventListener("click", ()=>{
-  sidebar.classList.toggle("open");
-  menuBtnChange();//calling the function(optional)
-});
-
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
-
-// following are the code to change sidebar button(optional)
-function menuBtnChange() {
- if(sidebar.classList.contains("open")){
-   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
- }else {
-   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
- }
-}
-
-
 document.getElementById("scroll-to-bottom-button").addEventListener("click", function() {
-
     document.body.scrollIntoView({ behavior: "smooth", block: "end" });
+});
+
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
     
-    });
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
