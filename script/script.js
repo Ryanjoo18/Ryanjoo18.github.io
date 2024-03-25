f3ed3a1f8333ea1460b210f2d90da92ffcf13e57
Ryan Joo <ryanjooruian18@gmail.com>
2024-03-25 19:03:22 +0800

    Update CV

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* Scrollbar */
 document.getElementById("scroll-to-bottom-button").addEventListener("click", function() {
     document.body.scrollIntoView({ behavior: "smooth", block: "end" });
 });
@@ -11,3 +12,23 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
