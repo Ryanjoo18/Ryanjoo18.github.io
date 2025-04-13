99aa31c508c8635cdc644aa0fbdf04933cd9872f
Ryan <ryanjooruian18@gmail.com>
2025-04-13 20:55:25 +0800

    Add initial HTML files for notes and projects sections
    - Created notes.html with links to A-Level and O-Level subjects.
    - Created projects.html featuring various projects with descriptions and links.
    - Added individual project pages: fires-hotspots.html, happiness.html, infinity.html, maths-olympiad.html, and undergrad-maths.html.
    - Each project page includes download links and descriptions of the work.

--- script/script.js
+++ script/script.js
@@ -2,13 +2,3 @@
 document.getElementById("scroll-to-bottom-button").addEventListener("click", function() {
     document.body.scrollIntoView({ behavior: "smooth", block: "end" });
 })
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function navButton() {
-  var x = document.getElementById("nav-menu-links");
-  if (x.className === "nav-menu-links") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-menu-links";
-  }
-}
